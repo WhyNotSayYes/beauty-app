57bd6304be41b762ff3cb081226c7976f7a61268
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-07-08 14:38:18 +0300

    init

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
         centerPadding: true,
         variableWidth: true,
         // infinite: false,
+        initialSlide: 1,
+        dots: true,
+        asNavFor: '.slider__descr',
     });
+    $('.slider__descr').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        arrows: false,
+        draggable: false,
+        swipe: false,
+        fade: true,
+        cssEase: 'linear',
+    });
+
 })
 
